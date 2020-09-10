4c2840d42c2e47330a0553bafcd1933a4dac1fa6
pankajsre <pankaj.saini85@gmail.com>
2020-09-10 15:18:22 +0530

    servlet changed

--- src/main/java/com/google/cloud/HelloAppEngine.java
+++ src/main/java/com/google/cloud/HelloAppEngine.java
@@ -7,10 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(
-    name = "HelloAppEngine",
-    urlPatterns = {"/hello"}
-)
+@WebServlet("/hello")
 public class HelloAppEngine extends HttpServlet {
 
   @Override
